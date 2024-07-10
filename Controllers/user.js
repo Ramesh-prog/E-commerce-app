9f4cc5fe796ec5301fa87627603ffd00008de9fa
Ramesh Mohapatra <rameshmohapatra2078@gmail.com>
2024-07-10 17:03:02 +0530

    user api finished

--- Controllers/user.js
+++ Controllers/user.js
@@ -1,11 +1,46 @@
 import { User } from "../Models/User.js";
+import bcrypt from "bcryptjs";
 
-export const register = async (req,res) => {
-    const {name, email, password } = req.body;
-    try {
-        let user = await User.create({name, email, password});
-        res.json({message: "user register successfully...", success:true })
-    } catch (error) {
-        res.json({message:error.message})
-    }
+// user register
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (user)
+      return res.json({ message: "user already exist", success: false });
+    const hashPass = await bcrypt.hash(password, 10);
+
+    user = await User.create({ name, email, password });
+    res.json({ message: "user register successfully...", success: true });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
 };
+
+// user login
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let user = await User.findOne({ email });
+    if (!user) return res.json({ message: "user not found", success: false });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword)
+      return res.json({ message: "invalid Credential", success: false });
+    res.json({ message: `welcome ${user.name}`, success: true, user });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+};
+
+// get all user
+
+export const users = async (req,res) => {
+    try{
+        let users = await User.find().sort({createdAt:-1});
+        res.json(users)
+    } catch (error) {
+        res.json(error.message)
+    }
+}
--- Routes/user.js
+++ Routes/user.js
@@ -1,10 +1,18 @@
 import express from "express"
-import { register } from "../Controllers/user.js"
+import { register , login, users} from "../Controllers/user.js"
 
 const router = express.Router();
 
 // register user
 
-router.post('/register', register)
+router.post('/register', register) // /api/user/register
+
+// login user
+
+router.post('/login', login) // /api/user/login
+
+// get all user
+
+router.get('/users', users) // /api/user/users
 
 export default router
